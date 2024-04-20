cd2ef267673343629d20704e25eaebbaed90b7d7
ángel30147 <angelcastro30147754@gmail.com>
2024-04-20 17:36:54 -0400

    RemplazoCV

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
-const { name } = require('ejs');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  console.log("asasasd")
-
-  res.render('index', {
-    title: 'Hola Mundo',
-    name: 'Angel Jose',
-    lastname: 'Castro Ramirez',
-    dni: '30.147.754',
-    section: '2',
-  });
+  res.render('index', { title: 'Angel Jose Castro Ramirez CV' });
 });
 
 module.exports = router;
