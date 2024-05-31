407c14e1be94d8b191b31c6f0d5cd9853d32fe1b
Angel Jose Castro Ramirez <165974551+Angel30147@users.noreply.github.com>
2024-05-30 22:27:50 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,48 +2,111 @@ var express = require('express');
 var router = express.Router();
 const sqlite3 = require("sqlite3").verbose();
 const path = require('path');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-class ContactosModel{
-  constructor(){
+
+class ContactosModel {
+  constructor() {
     this.db = new sqlite3.Database(path.join(__dirname, "/basededatos", "base.db"), (err) => {
-      if(err){
+      if (err) {
         console.log('Error en la base de datos')
       }
       console.log('Base de datos creada')
     });
   }
 
-  conectardb(){
-    this.db.run('CREATE TABLE IF NOT EXISTS contactos(nombre VARCHAR(255), correo VARCHAR(255), comentario TEXT,ip TEXT,fecha TEXT)');
-}
-  
-  guardardb(name,email,commentary,ip,fecha){
-    this.db.run("INSERT INTO contactos VALUES (?, ?, ?, ?, ?)", [name, email, commentary, ip, fecha]);
+  conectardb() {
+    this.db.run('CREATE TABLE IF NOT EXISTS contactos(nombre VARCHAR(255), correo VARCHAR(255), comentario TEXT,ip TEXT,fecha TEXT, country TEXT)');
+  }
+
+  guardardb(name, email, commentary, ip, fecha, country) {
+    this.db.run("INSERT INTO contactos VALUES (?, ?, ?, ?, ?, ?)", [name, email, commentary, ip, fecha, country]);
   }
 }
 
-class ContactosController{
-  constructor(){
+class ContactosController {
+  constructor() {
     this.modeloFacade = new ContactosModel();
     this.modeloFacade.conectardb();
   }
 
-  saveForm(req,res){
-    const { name, email, commentary} = req.body;
+  async saveForm(req, res) {
+    const { name, email, commentary } = req.body;
     const ip = req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
     const fecha = new Date().getTime();
-    this.modeloFacade.guardardb(name,email,commentary,ip,fecha);
-    res.send('Formulario guardado')
-    
+
+    const url = 'http://ipwho.is/' + ip;
+    const responseCountry = await fetch(url);
+    const jsonCountry = await responseCountry.json();
+    const country = jsonCountry.country;
+
+    const responseGoogle = req.body["g-recaptcha-response"];
+    const secretGoogle = process.env.PRIVATEKEY;
+    const urlG = `https://www.google.com/recaptcha/api/siteverify?secret=${secretGoogle}&response=${responseGoogle}`;
+    const recaptchaGoogle = await fetch(urlG, { method: "post", });
+    const googleResult = await recaptchaGoogle.json();
+
+    if (googleResult.success) {
+
+      let transporter = nodemailer.createTransport({
+        host: "smtp.hostinger.com",
+        port: 465,
+        secureConnection: false,
+        auth: {
+          user: process.env.EMAILTEST,
+          pass: process.env.PASSWORDTEST
+        },
+        debug: true
+      });
+
+      const customer = `
+
+        <p>Email: ${email}</p>
+        <p>Name: ${name}</p>
+        <p>Message: ${commentary}</p>
+        <p>Date Time: ${fecha}</p>
+        <p>Country: ${country}</p>
+        <p>Ip: ${ip}</p>
+                            `;
+
+      const receiver = {
+        from: process.env.EMAILTEST,
+        to: 'programacion2ais@dispostable.com',
+        subject: 'Data client',
+        html: customer
+      };
+
+
+      transporter.sendMail(receiver, (err, info) => {
+        if (err) {
+          console.log(err)
+        }
+
+        else {
+          this.modeloFacade.guardardb(name, email, commentary, ip, fecha, country);
+          res.send('Formulario y correo enviado');
+        }
+
+      })
+
+
+
+
+    } else {
+      res.send('Error en el captcha')
+    }
+
+
   }
 }
 
 const controllerFacade = new ContactosController();
-router.post('/sendForm',(req,res,next) => controllerFacade.saveForm(req,res))
+router.post('/sendForm', (req, res, next) => controllerFacade.saveForm(req, res))
 
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Angel Jose Castro Ramirez CV' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Angel Jose Castro Ramirez CV', publicKey: process.env.PUBLICKEY});
 });
 
 module.exports = router;
